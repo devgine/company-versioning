d64581e04905b0a61d90e2989bf5b83e028d1246
Yosri BAHRI <yosribahri@gmail.com>
2023-05-01 00:27:21 +0200

    [webapp] setup rest api faker

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,12 @@
 import { Admin, Resource } from 'react-admin';
 import { Dashboard } from './Dashboard';
-import dataProvider from './common/dataProvider';
+import ApiDataProvider from './common/dataProvider';
+import FakeDataProvider from './common/dataProviderFaker';
 import companies from './resources/Company';
 import addresses from './resources/Address';
+
+const dataProvider =
+    import.meta.env.VITE_FAKER_REST == 0 ? ApiDataProvider : FakeDataProvider;
 
 const App = () => (
     <Admin dataProvider={dataProvider} dashboard={Dashboard}>
